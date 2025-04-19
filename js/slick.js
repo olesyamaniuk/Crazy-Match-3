39304f1ea086e455908e3e21aee6a817936ddaf1
olesyamaniuk <72089671+olesyamaniuk@users.noreply.github.com>
2025-04-19 14:47:42 +0300

    2

--- js/slick.js
+++ js/slick.js
@@ -1,5 +1,5 @@
 $(function () {
-  $(".class-gallery-list").slick({
+  $(".q-s-gallery-list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -10,13 +10,12 @@ $(function () {
     dots: false,
     centerMode: true,
     cssEase: "linear",
-    prevArrow: ".class-gallery-list-left",
-    nextArrow: ".class-gallery-list-right",
+    prevArrow: ".q-s-gallery-list-left",
+    nextArrow: ".q-s-gallery-list-right",
   });
 });
-
 $(function () {
-  $(".class-reviews-list").slick({
+  $(".q-s-why-list").slick({
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
@@ -24,10 +23,42 @@ $(function () {
     autoplay: true,
     autoplaySpeed: 3500,
     speed: 1000,
-    dots: true,
+    dots: false,
     centerMode: true,
     cssEase: "linear",
-    prevArrow: ".class-reviews-list-left",
-    nextArrow: ".class-reviews-list-right",
+    prevArrow: ".q-s-why-list-left",
+    nextArrow: ".q-s-why-list-right",
+  });
+});
+$(function () {
+  $(".q-s-how-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    dots: false,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".q-s-how-list-left",
+    nextArrow: ".q-s-how-list-right",
+  });
+});
+
+$(function () {
+  $(".q-s-reviews-list").slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    variableWidth: true,
+    autoplay: true,
+    autoplaySpeed: 3500,
+    speed: 1000,
+    centerMode: true,
+    cssEase: "linear",
+    prevArrow: ".q-s-reviews-list-left",
+    nextArrow: ".q-s-reviews-list-right",
   });
 });
